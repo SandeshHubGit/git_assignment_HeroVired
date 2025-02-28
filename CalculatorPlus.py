44315718cf2ea75ce30656f1aed5c5612f892299
SandeshHiubGit <sandeshgaikwad1994@gmail.com>
2025-02-28 16:55:30 +0530

    Test : Test in dev branch

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -1,3 +1,4 @@
+## Final Release
 import math
 class Calculator:
     def add(self, a, b):
@@ -8,6 +9,8 @@ class Calculator:
         return a * b
     def divide(self, a, b):
         return a / b
+    def square_root(self, x):
+        return math.sqrt(x)
     def divide(self, a, b):
         if b == 0:
             raise ValueError("Cannot divide by zero.")
@@ -26,3 +29,7 @@ if __name__ == "__main__":
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}"), print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+
+    num3 = 25
+
+    print(f"The square root of {num3} = {calculator.square_root(num3)}")
